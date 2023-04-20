b9ecfc91b1803d7eb3e5ab2c44d1ca6cf7aaac3f
yangyulu <yangyulu1114@163.com>
2023-04-20 09:23:03 -0700

    change comments-schema

--- comments/comments-schema.js
+++ comments/comments-schema.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 const commentsSchema = new mongoose.Schema(
     {
         user: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+        username: {type:String, required: true},
         trackId: {type:String, required: true},
         trackName: {type:String, required: true},
         comment: { type: String},
